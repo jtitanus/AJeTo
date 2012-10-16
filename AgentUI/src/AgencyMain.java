4cda5e4bd57cbda6fd62a0c3b1fcc25aedb979fb
jtitanus <jtitanus@github.com>
2012-10-16 16:49:50 +0200

    Added uml diagram, added a simple swing GUI for displaying the agents

---
+++ AgentUI/src/AgencyMain.java
@@ -0,0 +1,17 @@
+import java.awt.Dimension;
+import java.awt.Frame;
+
+
+public class AgencyMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		DrawingWindow w = new DrawingWindow( "Agency");
+		w.setVisible(true);
+		
+	}
+
+}
